c6ea68b370d560277caefea2152c965d1c5238a9
Alessandro Jean <alessandrojean@gmail.com>
2019-09-17 11:28:53 -0300

    Remove internationalization.

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,56 +1,23 @@
 module.exports = {
+	title: 'Tachiyomi',
+	description: 'Free and open source manga reader for Android',
 	dest: './public',
-	locales: {
-		'/': {
-			lang: 'en-US',
-			title: 'Tachiyomi',
-			description: 'Free and open source manga reader for Android'
-		},
-		'/pt/': {
-			lang: 'pt-BR',
-			title: 'Tachiyomi',
-			description: 'Leitor de mangás gratuito e de código aberto para Android'
-		}
-	},
 	themeConfig: {
 		repo: 'inorichi/tachiyomi',
 		docsRepo: 'tachiyomiorg/website',
 		docsDir: 'src',
 		editLinks: true,
-		locales: {
-			'/': {
-				selectText: 'Languages',
-				label: 'English',
-				nav: [
-					{ text: 'Guide', link: '/guide/' },
-					{ text: 'FAQ', link: '/faq/' },
-					{ text: 'Wiki', link: 'https://github.com/inorichi/tachiyomi/wiki' },
-					{ text: 'Discord', link: 'https://discord.gg/tachiyomi'}
-				],
-				sidebar: [
-					'/',
-					'/guide/',
-					'/faq/'
-				],
-				lastUpdated: 'Last Updated'
-			},
-			'/pt/': {
-				selectText: 'Idiomas',
-				label: 'Português (Brasil)',
-				nav: [
-					{ text: 'Guia', link: '/pt/guide/' },
-					{ text: 'FAQ', link: '/pt/faq/' },
-					{ text: 'Wiki', link: 'https://github.com/inorichi/tachiyomi/wiki' },
-					{ text: 'Discord', link: 'https://discord.gg/tachiyomi' }
-				],
-				sidebar: [
-					'/pt/',
-					'/pt/guide/',
-					'/pt/faq/'
-				],
-				editLinkText: 'Edite esta página',
-				lastUpdated: 'Atualizado em'
-			}
-		}
+		nav: [
+			{ text: 'Guide', link: '/guide/' },
+			{ text: 'FAQ', link: '/faq/' },
+			{ text: 'Wiki', link: 'https://github.com/inorichi/tachiyomi/wiki' },
+			{ text: 'Discord', link: 'https://discord.gg/tachiyomi' }
+		],
+		sidebar: [
+			'/',
+			'/guide/',
+			'/faq/'
+		],
+		lastUpdated: 'Last Updated'
 	}
 }
