3d1f9a42d0de23fe3f64c377905243051df6ccf6
Andreas E <andreas.everos@gmail.com>
2019-10-01 15:04:40 +0200

    Add custom .vue component for Material Icons (#64)
    * Add custom .vue component for Material Icons
    
    * Remove unnecessary line
    
    * Add class name to div

---
+++ src/.vuepress/components/MaterialIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="material-holder">
+    <i class="material-icons">{{ iconName }}</i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['iconName']
+}
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+
+.material-holder {
+    color: #476582;
+    background-color: rgba(27,31,35,0.05);    
+    width: fit-content;
+    display: inline;
+}
+
+.material-icons {
+    font-size: 18px;
+}
+</style>
