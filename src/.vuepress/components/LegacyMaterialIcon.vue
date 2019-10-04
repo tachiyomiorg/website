6637595ed3e19c6a39b70bf0f871797377931ff8
Andreas E <andreas.everos@gmail.com>
2019-10-04 14:07:11 +0200

    Add style

--- src/.vuepress/components/LegacyMaterialIcon.vue
+++ src/.vuepress/components/LegacyMaterialIcon.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="iconify-holder">
-    <i class="mdi" v-bind:class="iconName"></i>
+  <div class="legacy-material-holder">
+    <i class="legacy-material-icons mdi" v-bind:class="iconName"></i>
   </div>
 </template>
 
@@ -12,4 +12,16 @@ export default {
 
 <style scoped>
 @import url("https://cdn.materialdesignicons.com/4.4.95/css/materialdesignicons.min.css");
+
+.legacy-material-holder {
+    color: #476582;
+    margin: 0;
+    font-size: 0.85em;
+    border-radius: 3px;
+    display: inline;
+}
+
+.legacy-material-icons {
+    font-size: 1.35em;
+}
 </style>
