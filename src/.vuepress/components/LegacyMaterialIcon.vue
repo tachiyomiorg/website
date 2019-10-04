80cb46f7a961a3729ff49c12871b610785510bd9
Andreas E <andreas.everos@gmail.com>
2019-10-04 14:00:11 +0200

    Create LegacyMaterialIcon.vue

---
+++ src/.vuepress/components/LegacyMaterialIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="iconify-holder">
+    <i class="mdi" v-bind:class="iconName"></i>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['iconName']
+}
+</script>
+
+<style scoped>
+@import url("https://cdn.materialdesignicons.com/4.4.95/css/materialdesignicons.min.css");
+</style>
