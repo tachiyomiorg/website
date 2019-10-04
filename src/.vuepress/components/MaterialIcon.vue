455f45161d65e2bad45be220c82e1c5287b612a5
Andreas E <andreas.everos@gmail.com>
2019-10-04 16:21:16 +0200

    Create LegacyMaterialIcon.vue (#76)
    * Create LegacyMaterialIcon.vue
    
    * Add style
    
    * Add legacy icons to MaterialIcon
    
    * Change indentation
    
    * Remove MDI-Glasses

--- src/.vuepress/components/MaterialIcon.vue
+++ src/.vuepress/components/MaterialIcon.vue
@@ -1,29 +1,51 @@
 <template>
-  <div class="material-holder">
-    <i class="material-icons">{{ iconName }}</i>
-  </div>
+	<div class="icon-holder">
+		<i v-if="legacy" class="legacy-material-icons mdi" v-bind:class="iconName"></i>
+		<i v-else class="material-icons">{{ iconName }}</i>
+	</div>
 </template>
 
 <script>
+/** 
+ * For material icon references use https://material.io/resources/icons/
+ * Code example: <MaterialIcon icon-name="android" />
+ * 
+ * For material icon references use http://materialdesignicons.com/
+ * Code example: <MaterialIcon icon-name="mdi-glasses" legacy />
+*/
 export default {
-  props: ['iconName']
+	props: {
+		legacy: {
+			type: Boolean,
+			default: false
+		},
+		iconName: {
+			type: String,
+			required: true
+		}
+	}
 }
 </script>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+@import url("https://cdn.materialdesignicons.com/4.4.95/css/materialdesignicons.min.css");
 
-.material-holder {
-    color: #476582;
-    margin: 0;
-    font-size: 0.85em;
-    border-radius: 3px;
-    display: inline;
+.icon-holder {
+	color: #476582;
+	margin: 0;
+	font-size: 0.85em;
+	border-radius: 3px;
+	display: inline;
 }
 
 .material-icons {
-    font-size: 1.35em;
-    position: relative;
-    top: 0.2rem;
+	font-size: 1.35em;
+	position: relative;
+	top: 0.2rem;
+}
+
+.legacy-material-icons {
+	font-size: 1.35em;
 }
 </style>
