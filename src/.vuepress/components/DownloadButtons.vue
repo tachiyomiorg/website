cfa12fc884dc15efa481f6bac9f98d7b69f6214f
Simon Mattila <simon.mattila@protonmail.com>
2019-10-07 17:12:57 +0200

    Restyle designs and alignment

--- src/.vuepress/components/DownloadButtons.vue
+++ src/.vuepress/components/DownloadButtons.vue
@@ -1,94 +1,97 @@
 <template>
-	<ul class="noSelect">
+	<ul class="noSelect download-list">
 		<li>
-			<a class="action-button" rel="noopener noreferrer" :href="browserDownloadUrl || null" :title="tagName" download>
-				<div class="download stable">
-					<font-awesome-icon icon="download" />
-					<span class="df">Download latest</span>
-					<span class="dfn">Stable release</span>
+			<a class="download-link" rel="noopener noreferrer" :href="browserDownloadUrl || 'https://github.com/inorichi/tachiyomi/releases/latest'" :title="tagName" download>
+				<div class="download-button stable">
+					<span class="download-area">
+						<MaterialIcon name="download-get_app download-icons" icon-name="get_app"/>
+						<span class="download-text-stable download-text">Stable release</span>
+					</span>
 				</div>
 			</a>
 		</lI>
 		<li>
-			<a href="http://tachiyomi.kanade.eu/latest">
-				<div class="download dev">
-					<font-awesome-icon icon="bug" />
-					<span class="df">Download latest</span>
-					<span class="dfn">Dev release</span>
+			<a class="download-link" href="http://tachiyomi.kanade.eu/latest">
+				<div class="download-button dev">
+					<div class="download-area">
+						<MaterialIcon name="download-bug_report download-icons" icon-name="bug_report"/>
+						<span class="download-text-dev download-text">Dev release</span>
+					</div>
 				</div>
 			</a>
 		</li>
 	</ul>
 </template>
 
-<style scoped>
+<style scoped lang="stylus">
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+@import url(https://fonts.googleapis.com/css?family=Open+Sans);
+* {
+  font-family: 'Open Sans', 'sans-serif';
+}
+
 ul {
 	width: 100%;
 	text-align: center;
-	margin: 0;
+	margin: 10px 0 0 0;
 	padding: 0;
 }
 
 li {
 	display: inline-block;
-	margin: 10px;
+	margin: 5px 10px 5px 10px;
 }
 
-.download {
-	width: 200px;
-	height: 75px;
-	float: left;
-	border-radius: 5px;
-	position: relative;
-	cursor: pointer;
-}
-
-.download.stable {
-	background: #2E84BF;
-	color: #fff;
-}
-
-.download.dev {
-	background: #476582;
-	color: #fff;
-}
-
-.download > svg {
-	font-size: 32px;
-	color: #fff;
-	position: absolute;
-	top: 50%;
-	left: 20px;
-	height: 1em;
-	width: 1em;
-	transform: translateY(-50%);
-}
-
-.download:hover {
-	filter: brightness(110%);
-}
-
-.df,
-.dfn {
-	position: absolute;
-	left: 70px;
-}
-
-.df {
-	top: 20px;
-	font-size: .68em;
-}
-
-.dfn {
-	top: 33px;
-	font-size: 1.08em;
-}
-
-.noSelect {
-	-webkit-user-select: none;
-	-moz-user-select: none;
-	-ms-user-select: none;
-	user-select: none;
+a.download-link {
+	&:hover {
+		text-decoration: none;
+	}
+	.download-button {
+		display: flex;
+		cursor: pointer;
+		justify-content: center;
+		align-items: center;
+		background-color: #2E84BF;
+		width: 11em;
+		height: 3.7em;
+		border-radius: 5px;
+		padding: 0em 1em 0em 1em;
+		&.stable {
+			background-color: #2E84BF;
+			&:hover {
+				filter: brightness(110%);
+			}
+		}
+		&.dev {
+			background-color: #476582;
+			&:hover {
+				filter: brightness(110%);
+			}
+		}
+		.download-area {
+			font-size: 1.1em;
+			color: #FFFFFF;
+			display: flex;
+			align-items: center;
+			.download-icons {
+				color: #FFFFFF;
+				max-width: 32px;
+			}
+			.download-get_app {
+				margin-left: 3px;
+				margin-right: 4px;
+			}
+			.download-bug_report {
+				margin-right: 4px;
+			}
+			.download-text-stable {
+				margin-right: 6px;
+			}
+			.download-text-dev {
+				margin-right: 1px;
+			}
+		}
+	}
 }
 </style>
 
