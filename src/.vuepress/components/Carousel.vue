2f8814f5619586397ded3466d8e54b782bc9f4c6
Andreas E <andreas.everos@gmail.com>
2019-11-13 15:35:20 +0100

    Basic style

--- src/.vuepress/components/Carousel.vue
+++ src/.vuepress/components/Carousel.vue
@@ -2,6 +2,12 @@
 	<div class="carousel-root">
 		<agile class="carousel" :options="config" v-bind:id="name">
 			<slot />
+			<template slot="prevButton">
+				<MaterialIcon iconName="navigate_next" />
+			</template>
+    		<template slot="nextButton">
+				<MaterialIcon iconName="navigate_next" />
+			</template>
 		</agile>
 	</div>
 </template>
@@ -17,9 +23,8 @@ export default {
 		type: Object,
 		default: function () {
 			return {
-				navButtons: false,
-				centerMode: true,
-				infinite: true,
+				navButtons: true,
+				centerMode: true
 			}
 		}
 	}
@@ -33,10 +38,72 @@ export default {
 	border-radius: 1em;
 }
 
-.flickity-button {
+.agile__nav-button {
+		background: hsla(0,0%,100%,.75);
+		border: none;
+		color: rgba(44, 62, 80);
+		cursor: pointer;
+		font-size: 24px;
+		height: 50px;
+		top: 50% !important;
+		transform: translateY(-50%);
+		position: absolute;
+		top: 0;
+		transition-duration: .3s;
+		width: 50px;
+		border-radius: 99em;
+}
+
+.agile__nav-button:hover {
+	background-color: rgba(0,0,0, .5);
+	opacity: 1;
+}
+
+.agile__nav-button--prev {
+	left: 10px;
+}
+
+.agile__nav-button--prev i {
+	transform: rotateZ(180deg)
+}
+
+.agile__nav-button--next {
+	right: 10px;
+}
+
+.agile__dots {
+	bottom: 10px;
+	left: 50%;
 	position: absolute;
-	background: hsla(0,0%,100%,.75);
-	border: none;
-	color: rgba(44, 62, 80);
+	transform: translateX(-50%);
+}
+
+.agile__dot {
+	margin: 0 10px;
+}
+
+.agile__dot button {
+	background-color: hsla(0,0%,100%,.75);
+	border: 1px solid hsla(0,0%,100%,.75);
+	border-radius: 50%;
+	cursor: pointer;
+	display: block;
+	height: 10px;
+	font-size: 0;
+	line-height: 0;
+	margin: 0;
+	padding: 0;
+	transition-duration: .3s;
+	width: 10px;
+}
+
+.agile__dot--current button {
+	background-color: rgba(44, 62, 80);
+	border-color: rgba(44, 62, 80);
+}
+
+.agile__dot button:hover {
+	background-color: rgba(44, 62, 80);
+	border-color: rgba(44, 62, 80);
 }
 </style>
