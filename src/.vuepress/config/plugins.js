e9b7d6531af0e89c57d51533dc33931b11acb7d4
arkon <eugcheung94@gmail.com>
2020-01-08 18:28:09 -0500

    Configure automatic linting

--- src/.vuepress/config/plugins.js
+++ src/.vuepress/config/plugins.js
@@ -1,18 +1,39 @@
 module.exports = [
-	['clean-urls',	{
-		normalSuffix: '/' }],
-	['@vuepress/back-to-top'],
-	['@vuepress/google-analytics', {
-		'ga': 'UA-148212082-1' }],
-	['vuepress-plugin-container', {
-		type: 'expander',
-		before: info => `<details class="expander content"><summary class="expander expansion">${info}</summary>`,
-		after: '</details>'}],
-	['vuepress-plugin-container', {
-		type: 'guide',
-		before: info => `<div class="guide"><p class="title">${info}</p>`,
-		after: '</div>'}],
-	['vuepress-plugin-container', {
-		type: 'aside',
-		defaultTitle: ''}]
+	[
+		"clean-urls",
+		{
+			normalSuffix: "/"
+		}
+	],
+	["@vuepress/back-to-top"],
+	[
+		"@vuepress/google-analytics",
+		{
+			ga: "UA-148212082-1"
+		}
+	],
+	[
+		"vuepress-plugin-container",
+		{
+			type: "expander",
+			before: info =>
+				`<details class="expander content"><summary class="expander expansion">${info}</summary>`,
+			after: "</details>"
+		}
+	],
+	[
+		"vuepress-plugin-container",
+		{
+			type: "guide",
+			before: info => `<div class="guide"><p class="title">${info}</p>`,
+			after: "</div>"
+		}
+	],
+	[
+		"vuepress-plugin-container",
+		{
+			type: "aside",
+			defaultTitle: ""
+		}
+	]
 ];
