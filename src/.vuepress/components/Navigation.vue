75e1e324e9d2ec3de9e3b81a04c03d50180a35d9
Soitora <simon.mattila@protonmail.com>
2020-04-27 17:07:26 +0200

    Linting

--- src/.vuepress/components/Navigation.vue
+++ src/.vuepress/components/Navigation.vue
@@ -1,99 +1,98 @@
 <template>
 	<span class="app-navigation">
-		<MaterialIcon class="app-icon" :icon-name="icon"/>
-			<span class="app-label">{{ text }}</span>
-		<slot/>
+		<MaterialIcon class="app-icon" :icon-name="icon" />
+		<span class="app-label">{{ text }}</span>
+		<slot />
 	</span>
 </template>
 
 <script>
 export default {
-  props: {
-	// more | extension
-	item: {
-	  type: String,
-	  required: true
-	}
-  },
-  computed: {
-	icon() {
-	  return {
-		/* Menus */
-		library: "collections_bookmark",
-		updates: "new_releases",
-		history: "history",
-		sources: "explore",
-		more: "more_horiz",
-		/* Actions */
-		search: "search",
-		filter: "filter_list",
-		update_library: "refresh",
-		/* More */
-		downloaded_only: "cloud_off",
-		extensions: "extension",
-		download_queue: "get_app",
-		source_migration: "compare_arrows",
-		settings: "settings",
-		about: "info",
-		help: "help",
-		/* Settings */
-		settings_general: "tune",
-		settings_library: "collections_bookmark",
-		settings_reader: "chrome_reader_mode",
-		settings_downloads: "get_app",
-		settings_tracking: "autorenew",
-		settings_backup: "backup",
-		settings_security: "security",
-		settings_advanced: "code",
-		/* Library info */
-		bookmark: "favorite_border",
-		bookmarked: "favorite",
-		set_categories: "label",
-		share: "share",
-		webview: "public",
-	  }[this.item];
+	props: {
+		item: {
+			type: String,
+			required: true
+		}
 	},
-	text() {
-	  return {
-		/* Menus */
-		library: "Library",
-		updates: "Updates",
-		history: "History",
-		sources: "Sources",
-		more: "More",
-		/* Actions */
-		search: "Search",
-		filter: "Filter",
-		update_library: "Update library",
-		/* More */
-		downloaded_only: "Downloaded only",
-		extensions: "Extensions",
-		download_queue: "Download queue",
-		source_migration: "Source migration",
-		settings: "Settings",
-		about: "About",
-		help: "Help",
-		/* Settings */
-		settings_general: "General",
-		settings_library: "Library",
-		settings_reader: "Reader",
-		settings_downloads: "Downloads",
-		settings_tracking: "Tracking",
-		settings_backup: "Backup",
-		settings_security: "Security",
-		settings_advanced: "Advanced",
-		/* Library info */
-		bookmark: "Add to library",
-		bookmarked: "In library",
-		set_categories: "Set categories",
-		share: "Share",
-		webview: "WebView",
-		/* Sources */
-		latest: "LATEST",
-		browse: "BROWSE",
-	  }[this.item];
+	computed: {
+		icon() {
+			return {
+				/* Menus */
+				library: "collections_bookmark",
+				updates: "new_releases",
+				history: "history",
+				sources: "explore",
+				more: "more_horiz",
+				/* Actions */
+				search: "search",
+				filter: "filter_list",
+				update_library: "refresh",
+				/* More */
+				downloaded_only: "cloud_off",
+				extensions: "extension",
+				download_queue: "get_app",
+				source_migration: "compare_arrows",
+				settings: "settings",
+				about: "info",
+				help: "help",
+				/* Settings */
+				settings_general: "tune",
+				settings_library: "collections_bookmark",
+				settings_reader: "chrome_reader_mode",
+				settings_downloads: "get_app",
+				settings_tracking: "autorenew",
+				settings_backup: "backup",
+				settings_security: "security",
+				settings_advanced: "code",
+				/* Library info */
+				bookmark: "favorite_border",
+				bookmarked: "favorite",
+				set_categories: "label",
+				share: "share",
+				webview: "public"
+			}[this.item];
+		},
+		text() {
+			return {
+				/* Menus */
+				library: "Library",
+				updates: "Updates",
+				history: "History",
+				sources: "Sources",
+				more: "More",
+				/* Actions */
+				search: "Search",
+				filter: "Filter",
+				update_library: "Update library",
+				/* More */
+				downloaded_only: "Downloaded only",
+				extensions: "Extensions",
+				download_queue: "Download queue",
+				source_migration: "Source migration",
+				settings: "Settings",
+				about: "About",
+				help: "Help",
+				/* Settings */
+				settings_general: "General",
+				settings_library: "Library",
+				settings_reader: "Reader",
+				settings_downloads: "Downloads",
+				settings_tracking: "Tracking",
+				settings_backup: "Backup",
+				settings_security: "Security",
+				settings_advanced: "Advanced",
+				/* Library info */
+				bookmark: "Add to library",
+				bookmarked: "In library",
+				set_categories: "Set categories",
+				share: "Share",
+				webview: "WebView",
+				/* Sources */
+				latest: "LATEST",
+				browse: "BROWSE"
+			}[this.item];
+		}
 	}
-  }
 };
 </script>
 
