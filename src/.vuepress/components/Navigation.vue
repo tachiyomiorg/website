a02e156a156eb6978e142338e2fbfb055c8580a3
Soitora <simon.mattila@protonmail.com>
2020-04-27 21:27:32 +0200

    Turn computed properties into an array

--- src/.vuepress/components/Navigation.vue
+++ src/.vuepress/components/Navigation.vue
@@ -1,14 +1,14 @@
 <template>
-	<span v-if="link" class="app-navigation" title="App navigation">
-		<a class="app-link" :href="link">
-			<MaterialIcon class="app-icon" :icon-name="icon" />
-			<span class="app-label">{{ text }}</span>
+	<span v-if="nav.link" class="app-navigation" title="App navigation">
+		<a class="app-link" :href="nav.link">
+			<MaterialIcon class="app-icon" :icon-name="nav.icon" />
+			<span class="app-label">{{ nav.text }}</span>
 			<slot />
 		</a>
 	</span>
 	<span v-else class="app-navigation" title="App navigation">
-		<MaterialIcon class="app-icon" :icon-name="icon" />
-		<span class="app-label">{{ text }}</span>
+		<MaterialIcon class="app-icon" :icon-name="nav.icon" />
+		<span class="app-label">{{ nav.text }}</span>
 		<slot />
 	</span>
 </template>
@@ -25,92 +25,44 @@ export default {
 		}
 	},
 	computed: {
-		icon() {
+		nav() {
 			return {
 				/* Menus */
-				library: "collections_bookmark",
-				updates: "new_releases",
-				history: "history",
-				sources: "explore",
-				more: "more_horiz",
+				library: { icon:"collections_bookmark", text:"Library" },
+				updates: { icon:"new_releases", text:"Updates" },
+				history: { icon:"history", text:"History" },
+				sources: { icon:"explore", text:"Sources" },
+				more: { icon:"more_horiz", text:"More" },
 				/* Actions */
-				search: "search",
-				filter: "filter_list",
-				update_library: "refresh",
+				search: { icon:"search", text:"Search" },
+				filter: { icon:"filter_list", text:"Filter" },
+				update_library: { icon:"refresh", text:"Update library" },
 				/* More */
-				downloaded_only: "cloud_off",
-				extensions: "extension",
-				download_queue: "get_app",
-				source_migration: "compare_arrows",
-				settings: "settings",
-				about: "info",
-				help: "help",
+				downloaded_only: { icon:"cloud_off", text:"Downloaded only" },
+				extensions: { icon:"extension", text:"Extensions" },
+				download_queue: { icon:"get_app", text:"Download queue" },
+				source_migration: { icon:"compare_arrows", text:"Source migration" },
+				settings: { icon:"settings", text:"Settings" },
+				about: { icon:"info", text:"About" },
+				help: { icon:"help", text:"Help" },
 				/* Settings */
-				settings_general: "tune",
-				settings_library: "collections_bookmark",
-				settings_reader: "chrome_reader_mode",
-				settings_downloads: "get_app",
-				settings_tracking: "autorenew",
-				settings_backup: "backup",
-				settings_security: "security",
-				settings_advanced: "code",
+				settings_general: { icon:"tune", text:"General", link:"/help/guides/general" },
+				settings_library: { icon:"collections_bookmark", text:"Library" },
+				settings_reader: { icon:"chrome_reader_mode", text:"Reader", link:"/help/guides/reader" },
+				settings_downloads: { icon:"get_app", text:"Downloads", link:"/help/guides/downloads" },
+				settings_tracking: { icon:"autorenew", text:"Tracking", link:"/help/guides/tracking" },
+				settings_backup: { icon:"backup", text:"Backup", link:"/help/guides/backup" },
+				settings_security: { icon:"security", text:"Security" },
+				settings_advanced: { icon:"code", text:"Advanced", link:"/help/guides/advanced" },
 				/* Library info */
-				bookmark: "favorite_border",
-				bookmarked: "favorite",
-				set_categories: "label",
-				share: "share",
-				webview: "public",
-			}[this.item];
-		},
-		text() {
-			return {
-				/* Menus */
-				library: "Library",
-				updates: "Updates",
-				history: "History",
-				sources: "Sources",
-				more: "More",
-				/* Actions */
-				search: "Search",
-				filter: "Filter",
-				update_library: "Update library",
-				/* More */
-				downloaded_only: "Downloaded only",
-				extensions: "Extensions",
-				download_queue: "Download queue",
-				source_migration: "Source migration",
-				settings: "Settings",
-				about: "About",
-				help: "Help",
-				/* Settings */
-				settings_general: "General",
-				settings_library: "Library",
-				settings_reader: "Reader",
-				settings_downloads: "Downloads",
-				settings_tracking: "Tracking",
-				settings_backup: "Backup",
-				settings_security: "Security",
-				settings_advanced: "Advanced",
-				/* Library info */
-				bookmark: "Add to library",
-				bookmarked: "In library",
-				set_categories: "Set categories",
-				share: "Share",
-				webview: "WebView",
+				bookmark: { icon:"favorite_border", text:"Add to library" },
+				bookmarked: { icon:"favorite", text:"In library" },
+				set_categories: { icon:"label", text:"Set categories" },
+				share: { icon:"share", text:"Share" },
+				webview: { icon:"public", text:"WebView" },
 				/* Sources */
-				latest: "LATEST",
-				browse: "BROWSE",
-			}[this.item];
-		},
-		link() {
-			return {
-				/* Settings */
-				settings_general: "/help/guides/general/",
-				settings_reader: "/help/guides/reader",
-				settings_downloads: "/help/guides/downloads",
-				settings_tracking: "/help/guides/tracking",
-				settings_backup: "/help/guides/backup",
-				settings_advanced: "/help/guides/advanced",
+				latest: { text:"LATEST" },
+				browse: { text:"BROWSE" },
 			}[this.item];
 		}
 	}
@@ -126,7 +78,7 @@ export default {
 		font-weight 500
 		font-size 1em
 	.app-icon
-		vertical-align middle
+		vertical-align top
 	.app-link
 		&:hover
 			cursor pointer
