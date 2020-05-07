246a6d966f59ef28477a116b7d5a9ffe24cdccba
Soitora <simon.mattila@protonmail.com>
2020-05-07 11:30:59 +0200

    Fix quote symbol

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -8,15 +8,15 @@ module.exports = {
 	// prettier-ignore
 	head: [
 		// PWA headers
-		["link", { rel: "icon", href: "/favicon.ico" }],
-		["link", { rel: "manifest", href: "/manifest.json" }],
-		["meta", { name: "theme-color", content: "#2e84bf" }],
-		["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
-		["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
-		["link", { rel: "apple-touch-icon", href: "/icons/apple-touch-icon-152x152.png" }],
-		["link", { rel: "mask-icon", href: "/icons/safari-pinned-tab.svg", color: "#2e84bf" }],
-		["meta", { name: "msapplication-TileImage", content: "/icons/msapplication-icon-144x144.png" }],
-		["meta", { name: "msapplication-TileColor", content: "#2e84bf" }]
+		['link', { rel: 'icon', href: '/favicon.ico' }],
+		['link', { rel: 'manifest', href: '/manifest.json' }],
+		['meta', { name: 'theme-color', content: '#2e84bf' }],
+		['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+		['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+		['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+		['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#2e84bf' }],
+		['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+		['meta', { name: 'msapplication-TileColor', content: '#2e84bf' }]
 		// Custom headers
 		['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin:''}],
 		['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans'}],
