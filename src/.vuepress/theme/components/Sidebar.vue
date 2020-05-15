c03c4ef6951544af02bb6c965be5fd9a6d91424f
Soitora <simon.mattila@protonmail.com>
2020-05-15 14:07:24 +0200

    Fix indentations

--- src/.vuepress/theme/components/Sidebar.vue
+++ src/.vuepress/theme/components/Sidebar.vue
@@ -1,15 +1,15 @@
 <template>
-  <aside class="sidebar">
-    <NavLinks />
+	<aside class="sidebar">
+		<NavLinks />
 
-    <slot name="top" />
+		<slot name="top" />
 
-    <SidebarLinks
-      :depth="0"
-      :items="items"
-    />
-    <slot name="bottom" />
-  </aside>
+		<SidebarLinks
+			:depth="0"
+			:items="items"
+		/>
+		<slot name="bottom" />
+	</aside>
 </template>
 
 <script>
@@ -17,50 +17,50 @@ import SidebarLinks from '@theme/components/SidebarLinks.vue'
 import NavLinks from './NavLinks.vue'
 
 export default {
-  name: 'Sidebar',
+	name: 'Sidebar',
 
-  components: { SidebarLinks, NavLinks },
+	components: { SidebarLinks, NavLinks },
 
-  props: ['items']
+	props: ['items']
 }
 </script>
 
 <style lang="stylus">
 .sidebar
-  ul
-    padding 0
-    margin 0
-    list-style-type none
-  a
-    display inline-block
-  .nav-links
-    display none
-    border-bottom 1px solid $borderColor
-    padding 0.5rem 0 0.75rem 0
-    a
-      font-weight 600
-    .nav-item
-      display block
-      line-height 1.25rem
-      font-size 1.1em
-      padding 0.5rem 0 0.5rem 1.5rem
-  & > .sidebar-links
-    padding 1.5rem 0
-    & > li > a.sidebar-link
-      font-size 1.1em
-      line-height 1.7
-      font-weight bold
-    & > li:not(:first-child)
-      margin-top .75rem
+	ul
+		padding 0
+		margin 0
+		list-style-type none
+	a
+		display inline-block
+	.nav-links
+		display none
+		border-bottom 1px solid $borderColor
+		padding 0.5rem 0 0.75rem 0
+		a
+			font-weight 600
+		.nav-item
+			display block
+			line-height 1.25rem
+			font-size 1.1em
+			padding 0.5rem 0 0.5rem 1.5rem
+	& > .sidebar-links
+		padding 1.5rem 0
+		& > li > a.sidebar-link
+			font-size 1.1em
+			line-height 1.7
+			font-weight bold
+		& > li:not(:first-child)
+			margin-top .75rem
 
 @media (max-width: $MQMobile)
-  .sidebar
-    .nav-links
-      display block
-      .dropdown-wrapper .nav-dropdown .dropdown-item a.router-link-active::after
-        top calc(1rem - 2px)
-      .nav-item
-        padding 0 0 0 1.5rem
-    & > .sidebar-links
-      padding 1rem 0
+	.sidebar
+		.nav-links
+			display block
+			.dropdown-wrapper .nav-dropdown .dropdown-item a.router-link-active::after
+				top calc(1rem - 2px)
+			.nav-item
+				padding 0 0 0 1.5rem
+		& > .sidebar-links
+			padding 1rem 0
 </style>
