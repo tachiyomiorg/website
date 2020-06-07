5ab29db2052c79fcdd6f53d5cd12f1d720088687
Andreas E <andreas.everos@gmail.com>
2020-06-07 15:37:06 +0200

    Check if browser support service worker

--- src/.vuepress/public/scripts/remove_service_worker.js
+++ src/.vuepress/public/scripts/remove_service_worker.js
@@ -1,11 +1,13 @@
-console.log("Checking for service worker...");
-navigator.serviceWorker.getRegistrations().then(function (registrations) {
-	if (registrations.length < 1) {
-		console.log("Found no service worker!");
-		return;
-	}
-	registrations.forEach((registration) => {
-		registration.unregister();
-		console.log("Removed service worker!");
+if ("serviceWorker" in navigator) {
+	console.log("Checking for service worker...");
+	navigator.serviceWorker.getRegistrations().then(function (registrations) {
+		if (registrations.length < 1) {
+			console.log("Found no service worker!");
+			return;
+		}
+		registrations.forEach((registration) => {
+			registration.unregister();
+			console.log("Removed service worker!");
+		});
 	});
-});
+}
