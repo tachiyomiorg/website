07a73407675b2a9cc013ab9e5c85a411129e5e38
Andreas E <andreas.everos@gmail.com>
2020-06-07 14:15:26 +0200

    Clean-up

--- src/.vuepress/public/scripts/remove_service_worker.js
+++ src/.vuepress/public/scripts/remove_service_worker.js
@@ -1,10 +1,10 @@
-console.log("Checking for service worker...")
-navigator.serviceWorker.getRegistrations().then(function(registrations) { 
-    if (registrations.length < 1) {
-        console.log("Found no service worker!")
-    }
-    for(let registration of registrations) { 
-        registration.unregister()
-        console.log("*yeet* Removed service worker!")
-    } 
-})
+console.log("Checking for service worker...");
+navigator.serviceWorker.getRegistrations().then(function (registrations) {
+	if (registrations.length < 1) {
+		console.log("Found no service worker!");
+	}
+	registrations.forEach((registration) => {
+		registration.unregister();
+		console.log("Removed service worker!");
+	});
+});
