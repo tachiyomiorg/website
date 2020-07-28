91c576ccda61aa19df4a847c090f5d9f4f6293fb
Soitora <simon.mattila@protonmail.com>
2020-07-29 01:46:48 +0200

    Create global constants file

---
+++ src/.vuepress/constants.js
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+export const githubExtensionJson = "https://raw.githubusercontent.com/inorichi/tachiyomi-extensions/repo/index.json"
+export const githubLatestRelease = "https://github.com/inorichi/tachiyomi/releases/latest";
+export const githubLatestApi = "https://api.github.com/repos/inorichi/tachiyomi/releases/latest";
+export const kanadeLatest = "https://tachiyomi.kanade.eu/latest";
