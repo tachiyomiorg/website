a34224f8478892e3fc3360200dd459068aa40417
Soitora <simon.mattila@protonmail.com>
2020-09-15 04:25:00 +0200

    Make the regex lazy
    So it doesn't get greedy and try to match from the first parenthesis to the very last.

--- src/.vuepress/components/WhatsNew.vue
+++ src/.vuepress/components/WhatsNew.vue
@@ -25,7 +25,7 @@ export default {
 		try {
 			const { data } = await this.$store.dispatch("getStableReleaseData");
 			this.$data.whatsNew = marked(data.body).replace(
-				/\(@(.*)\)/g,
+				/\(@(.*?)\)/g,
 				"(<a href='https://github.com/$1' target='_blank' rel='noopener'>@$1</a>)"
 			);
 		} catch (e) {
