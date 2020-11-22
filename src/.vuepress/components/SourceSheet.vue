864b683ac3c2d353b86444ac6c3870e7b6bf39dc
Soitora <simon.mattila@protonmail.com>
2020-11-22 22:12:22 +0100

    Initial commit

---
+++ src/.vuepress/components/SourceSheet.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="sourceSheet">
+		<tr v-for="item in items" :key="item.Id">
+			<td>{{ item.Extension }}</td>
+			<td>{{ item.Source }}</td>
+			<td>
+				<a :href="'https://' + item.Website">{{ item.Website }}</a>
+			</td>
+			<td>{{ item.Languages }}</td>
+			<td>{{ item.Notes }}</td>
+		</tr>
+	</div>
+</template>
+
+<script>
+import { vueGsheets } from "vue-gsheets";
+
+export default {
+	mixins: [vueGsheets],
+	date: () => ({
+		COLUMNS: 7,
+		sheetPageNumber: 1,
+		SHEETID: "1e4a9pTdLUmjzFvpfFr5QvGhhe_nRl3FI2kGt8ytFS0I",
+	}),
+}
+</script>
---
+++ src/extensions/spreadsheet.md
@@ -0,0 +1,11 @@
+---
+title: Spreadsheet
+description: Here is a list of all available extensions to download inside the app.
+lang: en-US
+---
+
+# Spreadsheet
+
+Here is a spreadsheet by Doc
+
+<SourceSheet/>
