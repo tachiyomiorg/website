f156ced1a02370407928a45128c19a610d82350f
Soitora <simon.mattila@protonmail.com>
2021-03-16 20:58:02 +0100

    Try another method of making routes

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -11,6 +11,21 @@ import Element from "element-ui";
 
 import store from "./store";
 
+const redirectList = [
+	{
+		path: "/help/guides/troubleshooting-problems/",
+		redirect: "/help/guides/troubleshooting/",
+	},
+	{
+		path: "/help/guides/reading-local-manga/",
+		redirect: "/help/guides/local-manga/",
+	},
+	{
+		path: "/help/guides/creating-backups/",
+		redirect: "/help/guides/backups/",
+	}
+]
+
 export default ({
 	Vue, // the version of Vue being used in the VuePress app
 	options, // the options for the root Vue instance
@@ -26,16 +41,5 @@ export default ({
 	Vue.use(Element);
 	Vue.mixin({ store });
 	// Reroute to new pages
-	router.beforeEach((to, from, next) => {
-		const redirectList = {
-			"/help/guides/troubleshooting-problems/": "/help/guides/troubleshooting/",
-			"/help/guides/reading-local-manga/": "/help/guides/local-manga/",
-			"/help/guides/creating-backups/": "/help/guides/backups/",
-		};
-		const redirect = redirectList[to.path];
-
-		if (redirect) {
-			next({ path: redirect });
-		} else next();
-	});
+	router.addRoutes(redirectList);
 };
