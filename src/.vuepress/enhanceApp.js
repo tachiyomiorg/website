18add6a63d485d01c08c0885e074b4091ca30ee1
Soitora <simon.mattila@protonmail.com>
2021-03-18 14:01:36 +0100

    Fix redirects (#573)
    * Fix redirects, maybe?
    
    Maybe fix redirects?
    
    * Change how pages are redirected
    
    * Remove 'redirect-frontmatter' reference
    
    * Try another method of making routes
    
    * Try dehydration
    
    * Update package-lock after rebase

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -11,6 +11,21 @@ import Element from "element-ui";
 
 import store from "./store";
 
+const redirectList = [
+	{
+		path: "/help/guides/troubleshooting-problems/",
+		redirect: "/help/guides/troubleshooting/",
+	},
+	{
+		path: "/help/guides/reading-local-manga/",
+		redirect: "/help/guides/local-manga/",
+	},
+	{
+		path: "/help/guides/creating-backups/",
+		redirect: "/help/guides/backups/",
+	}
+]
+
 export default ({
 	Vue, // the version of Vue being used in the VuePress app
 	options, // the options for the root Vue instance
@@ -25,4 +40,6 @@ export default ({
 	Vue.use(VueMoment);
 	Vue.use(Element);
 	Vue.mixin({ store });
+	// Reroute to new pages
+	router.addRoutes(redirectList);
 };
