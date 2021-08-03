ff489d16a164ffb96aceff4fe6f87847245ed87c
Andreas E <andreas.everos@gmail.com>
2021-08-03 19:24:07 +0200

    Fix pt-br returning ()

--- src/.vuepress/scripts/languages.js
+++ src/.vuepress/scripts/languages.js
@@ -1,9 +1,29 @@
 import ISO6391 from "iso-639-1";
 
+const ALL = "All";
+const ALL_CODE = ALL.toLowerCase();
+const PT_BR_CODE = "pt-br";
+const PT_BR_SIMPLE_NAME = "Brazilian Portuguese";
+const PT_BR_LANG_NAME = `${PT_BR_SIMPLE_NAME} (Português Brasileiro)`;
+
 export function simpleLangName(code) {
-	return code === "all" ? "All" : ISO6391.getName(code);
+	code = code.toLowerCase();
+	if (code === ALL_CODE) {
+		return ALL;
+	} else if (code === PT_BR_CODE) {
+		return PT_BR_SIMPLE_NAME;
+	} else {
+		return `${ISO6391.getName(code)}`;
+	}
 }
 
 export function langName(code) {
-	return code === "all" ? "All" : `${ISO6391.getName(code)} (${ISO6391.getNativeName(code)})`;
+	code = code.toLowerCase();
+	if (code === ALL_CODE) {
+		return ALL;
+	} else if (code === PT_BR_CODE) {
+		return PT_BR_LANG_NAME;
+	} else {
+		return `${ISO6391.getName(code)} (${ISO6391.getNativeName(code)})`;
+	}
 }
