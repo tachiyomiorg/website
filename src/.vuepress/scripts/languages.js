660e600047352326a895901f251ee7094a52975b
Andreas E <andreas.everos@gmail.com>
2021-08-04 11:35:40 +0200

    Replace ISO-639-1 with Intl.DisplayNames

--- src/.vuepress/scripts/languages.js
+++ src/.vuepress/scripts/languages.js
@@ -1,29 +1,16 @@
-import ISO6391 from "iso-639-1";
-
-const ALL = "All";
-const ALL_CODE = ALL.toLowerCase();
-const PT_BR_CODE = "pt-br";
-const PT_BR_SIMPLE_NAME = "Brazilian Portuguese";
-const PT_BR_LANG_NAME = `${PT_BR_SIMPLE_NAME} (Português Brasileiro)`;
-
 export function simpleLangName(code) {
-	code = code.toLowerCase();
-	if (code === ALL_CODE) {
-		return ALL;
-	} else if (code === PT_BR_CODE) {
-		return PT_BR_SIMPLE_NAME;
-	} else {
-		return `${ISO6391.getName(code)}`;
+	if (code === "all") {
+		return "All";
 	}
+	const namesInEnglish = new Intl.DisplayNames(["en"], { type: "language" });
+	return namesInEnglish.of(code);
 }
 
 export function langName(code) {
-	code = code.toLowerCase();
-	if (code === ALL_CODE) {
-		return ALL;
-	} else if (code === PT_BR_CODE) {
-		return PT_BR_LANG_NAME;
-	} else {
-		return `${ISO6391.getName(code)} (${ISO6391.getNativeName(code)})`;
+	if (code === "all") {
+		return "All";
 	}
+	const namesInEnglish = new Intl.DisplayNames(["en"], { type: "language" });
+	const namesInNative = new Intl.DisplayNames([code], { type: "language" });
+	return `${namesInEnglish.of(code)} - ${namesInNative.of(code)}`;
 }
