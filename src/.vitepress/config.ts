807cf6a2c0765d8e5b203b32f06c1c79ff212978
Soitora <simon.mattila@protonmail.com>
2023-07-20 16:41:37 +0200

    Update config

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -37,15 +37,7 @@ export default ({ mode }) => {
 				"/forks/": sidebarGuide(),
 			},
 
-			footer: {
-				message:
-					"<strong><a href='https://www.apache.org/licenses/LICENSE-2.0' target='_blank'>Open-source Apache Licensed</a></strong> | <strong><a href='./privacy'>Privacy policy</a></strong>",
-				copyright: `Copyright © 2015 - ${new Date().getFullYear()} Javier Tomás`,
-			},
-
-			search: {
-				provider: "local",
-			},
+			outline: [2, 3],
 
 			socialLinks: [
 				{ icon: "github", link: "https://github.com/tachiyomiorg/tachiyomi" },
@@ -53,6 +45,28 @@ export default ({ mode }) => {
 				{ icon: "facebook", link: "https://facebook.com/tachiyomiorg" },
 				{ icon: "discord", link: "https://discord.gg/tachiyomi" },
 			],
+
+			footer: {
+				message: "<strong><a href='https://www.apache.org/licenses/LICENSE-2.0' target='_blank'>Open-source Apache Licensed</a></strong> | <strong><a href='./privacy'>Privacy policy</a></strong>",
+				copyright: `Copyright © 2015 - ${new Date().getFullYear()} Javier Tomás`,
+			},
+
+			editLink: {
+				pattern: "https://github.com/xhenos/kodo/edit/v3-vitepress/src/:path",
+				text: "Help us improve this page",
+			},
+
+			lastUpdated: {
+				text: "Last updated",
+				formatOptions: {
+					dateStyle: "long",
+					timeStyle: "short",
+				},
+			},
+
+			search: {
+				provider: "local",
+			},
 		},
 	});
 };
