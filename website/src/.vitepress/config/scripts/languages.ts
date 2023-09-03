489801174ec0d12546042aabf86b20a72b094f48
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2023-09-03 16:56:00 -0300

    Capitalize the language name in some cases (#14)

--- website/src/.vitepress/config/scripts/languages.ts
+++ website/src/.vitepress/config/scripts/languages.ts
@@ -3,14 +3,19 @@ export function simpleLangName(code: string) {
 		return "All";
 	}
 	const namesInEnglish = new Intl.DisplayNames(["en"], { type: "language" });
-	return namesInEnglish.of(code)!;
+	return capitalize(namesInEnglish.of(code)!, "en");
 }
 
 export function langName(code: string) {
 	if (code === "all") {
 		return "All";
 	}
+
 	const namesInEnglish = new Intl.DisplayNames(["en"], { type: "language" });
 	const namesInNative = new Intl.DisplayNames([code], { type: "language" });
-	return `${namesInEnglish.of(code)} - ${namesInNative.of(code)}`;
+	return `${capitalize(namesInEnglish.of(code)!, "en")} - ${capitalize(namesInNative.of(code)!, code)}`;
+}
+
+function capitalize(string: string, locale: string) {
+	return string.charAt(0).toLocaleUpperCase(locale) + string.substring(1);
 }
