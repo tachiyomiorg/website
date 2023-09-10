dd82cdcbb7bea12a89bfc7760ce98530c987da1b
arkon <eugcheung94@gmail.com>
2023-09-10 17:57:15 -0400

    Remove Vuepress site

--- src/.vuepress/config/navBar.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-	{ text: "Home", link: "/" },
-	{ text: "Help Center", link: "/help/" },
-	{ text: "Download", link: "/download/" },
-	{ text: "Extensions", link: "/extensions/" },
-];
--- src/.vuepress/constants.js
+++
@@ -1,6 +0,0 @@
-export const GITHUB_EXTENSION_JSON =
-	"https://raw.githubusercontent.com/tachiyomiorg/tachiyomi-extensions/repo/index.json";
-export const GITHUB_STABLE_API = "https://api.github.com/repos/tachiyomiorg/tachiyomi/releases/latest";
-export const GITHUB_STABLE_RELEASE = "https://github.com/tachiyomiorg/tachiyomi/releases/latest";
-export const GITHUB_PREVIEW_API = "https://api.github.com/repos/tachiyomiorg/android-app-preview/releases/latest";
-export const GITHUB_PREVIEW_RELEASE = "https://github.com/tachiyomiorg/android-app-preview/releases/latest";
